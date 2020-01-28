bbaf5cb0e76b1edf4fb7a06accb9e7e60e2541a8
k.abhay84@gmail.com <k.abhay84@gmail.com>
2020-01-28 11:32:06 -0800

    secong practice

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -3,11 +3,17 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class WelcomeController {
+
+    @Value("${welcome.message}")
+    private String message;
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+
+        return message;
     }
 }
